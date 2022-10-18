b929f38feb7c67d411bea09cbb439e0161263dba
Anas Shaikh <itsanas1554@gmail.com>
2022-10-18 17:24:19 +0530

    Add files via upload

---
+++ Mission-X.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sleep 2
+
+sudo apt install wget -y
+
+sudo apt install git -y
+
+sudo apt install nano -y
+
+sudo apt install vim -y 
+
+sudo apt install htop -y
+
+sudo apt install curl -y
+
+sudo apt install fail2ban -y
+
+mkdir Crypto
+
+cd Crypto
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+
+tar -xzvf xmrig-6.18.0-linux-x64.tar.gz
+
+rm xmrig-6.18.0-linux-x64.tar.gz
+
+cd xmrig-6.18.0
+
+./xmrig
